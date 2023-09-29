8b1cf5a935f59482b5ae8c85c7724c64311d6b40
Juan Ignacio Mendez <juan.mendez@mi.unc.edu.ar>
2023-09-29 18:50:10 -0300

    models import User and Game too

--- src/position_repository.py
+++ src/position_repository.py
@@ -1,10 +1,10 @@
-from models import Position
+from models import Position, User, Game
 from pony.orm import db_session
 
 class PositionRepository:
 
     @db_session
-    def create_position(self, user, game, turn):
+    def create_position(self, user: User, game: Game, turn):
         Position(user=user, game=game, turn=turn)
 
     
