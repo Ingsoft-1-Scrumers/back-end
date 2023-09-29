65d9c4ff5097dfbb10dfd47b6c95f7199c4762aa
Juan Ignacio Mendez <juan.mendez@mi.unc.edu.ar>
2023-09-29 19:09:54 -0300

    get_position

--- src/position_repository.py
+++ src/position_repository.py
@@ -7,4 +7,9 @@ class PositionRepository:
     def create_position(self, user: User, game: Game, turn):
         Position(user=user, game=game, turn=turn)
 
-    
+    @db_session
+    def get_position(user: User) -> dict:
+        position = Position.get(user=user)
+        if position is None:
+            raise ValueError("Position does not exist")
+        return {'username': user.name, 'position': position.id}
